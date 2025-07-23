f0c9a3bb87483e86a3480b5e9d54232881924130
Sudip-Chel <sudipchel005@gmail.com>
2025-07-23 21:16:05 +0530

    maxiProductSubarray

---
+++ Array/maximumProductSubarray.cpp
@@ -0,0 +1,52 @@
+/*
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any subarray of nums is guaranteed to fit in a 32-bit integer.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = INT_MIN;
+
+        int prefix = 1 , suffix = 1;
+
+        for(int i = 0 ; i < nums.size() ; i++){
+
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+
+            maxi = max(maxi , max(prefix , suffix));
+
+        }
+        
+        return maxi;
+    }
+};
